b6dd91a7914498f495246a3152b1ba8d12c0629d
PierreGaudichon <pierre@gaudichon.com>
2014-05-30 17:45:13 +0200

    Create the CLI , app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+var fs = require("fs");
+var path = require("path");
+var commander = require("commander");
+var wh = require("./whirlpool");
+
+
+var hashFile = function(fileName){
+	fs.readFile(fileName, function(err, data){
+		if (err) { throw err; }
+		str = data.toString("utf8");
+		hashString(str);
+	});
+};
+
+var hashString = function(string){
+	console.log(wh.hash(string));
+};
+
+
+
+
+var version = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'))).version;
+
+commander
+	.version(version)
+	.usage("[options] string")
+	.option("-f, --file [file]", "Hash the content of a file.")
+	.parse(process.argv)
+	.name = "whirlpool";
+
+
+if (commander.file) {
+	hashFile(commander.file);
+}
+else if (commander.args.length) {
+	for (var i = 0; i < commander.args.length; i++) {
+		hashString(commander.args[i]);
+	};
+}
+else {
+	commander.help();
+}
